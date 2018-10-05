786495f23a4d8677c1ae333200da65b0de892268
giginet <giginet.net@gmail.com>
2018-10-05 17:33:41 +0900

    Implement LiteralParser

--- Sources/MinSwift/Parser.swift
+++ Sources/MinSwift/Parser.swift
@@ -8,19 +8,78 @@ internal indirect enum SyntaxTree {
     case subtraction(SyntaxTree, SyntaxTree)
 }
 
+internal struct Substructure: Codable {
+    enum Kind: String, Codable {
+        case variable = "source.lang.swift.decl.var.global"
+        case call = "source.lang.swift.expr.call"
+        case function = "source.lang.swift.decl.function.free"
+        case parameter = "source.lang.swift.decl.var.parameter"
+    }
+    
+    enum CodingKeys: String, CodingKey {
+        case kind = "key.kind"
+        case name = "key.name"
+        case substructures = "key.substructure"
+    }
+    
+    let kind: Kind
+    let name: String
+    let substructures: [Substructure]?
+}
+
 internal struct ParseResult {
 }
 
 internal struct Parser {
     init() { }
 
-    func parse(_ contents: String) -> [SyntaxTree] {
-        let statements = contents.split(separator: "\n")
-        let structure = try! Structure(file: File(contents: contents))
-        return statements.map { parseLine(String($0)) }
+    func parse(_ contents: String) throws -> [SyntaxTree] {
+        let substructures = try parseSubstructures(contents)
+        let tree = substructures.map { substructure -> SyntaxTree in
+            switch substructure.kind {
+            case .function:
+                break
+            case .call:
+                break
+            case .parameter:
+                break
+            case .variable:
+                break
+            }
+            return .literal("42")
+        }
+        return tree
+    }
+    
+    func parseSubstructures(_ contents: String) throws -> [Substructure] {
+        let structure = try Structure(file: File(contents: contents))
+        let data = try JSONSerialization.data(withJSONObject: structure.dictionary["key.substructure"]!, options: [])
+        let decoder = JSONDecoder()
+        return try decoder.decode([Substructure].self, from: data)
     }
 
     private func parseLine(_ line: String) -> SyntaxTree {
         return .literal(line)
+    }
+}
+
+public protocol LiteralType {
+    static func parse(from string: String) -> Self
+}
+
+extension Int: LiteralType {
+    public static func parse(from string: String) -> Int {
+        return Int(string)!
+    }
+}
+
+internal struct LiteralParser {
+    func parse<Literal: LiteralType>(from literalString: String) -> Literal? {
+        let digitRegexp = try! NSRegularExpression(pattern: "^[0-9]+$", options: [])
+        let matches = digitRegexp.matches(in: literalString, options: [], range: NSRange(location: 0, length: literalString.utf16.count))
+        if !matches.isEmpty {
+            return Literal.parse(from: literalString)
+        }
+        return nil
     }
 }
--- Tests/MinSwiftTests/ParserTests.swift
+++ Tests/MinSwiftTests/ParserTests.swift
@@ -1,8 +1,23 @@
 import Foundation
 import XCTest
+@testable import MinSwift
 
 class ParserTests: XCTestCase {
-    func testFoo() {
-        XCTAssertEqual(1 + 1, 2)
+    private let parser = Parser()
+    
+    func testParseStructures() {
+        let contents =
+        """
+40 + 2
+let a = 42
+print(1 + 1)
+func fizzbuzz(n: int) -> string {
+    return "fizzbuzz"
+}
+print(fizzbuzz())
+print(a)
+"""
+        let substructures = try! parser.parse(contents)
+        print(substructures)
     }
 }
